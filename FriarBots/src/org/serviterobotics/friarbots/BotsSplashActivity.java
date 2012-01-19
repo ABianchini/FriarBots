57edbb542903d6ebbb347afc6aaf8676c879d297
Adriano Bianchini2 <animationbureau@gmail.com>
2012-01-19 06:35:26 -0800

    Changed min sdk to Gingerbread, Added splash screen activity

---
+++ FriarBots/src/org/serviterobotics/friarbots/BotsSplashActivity.java
@@ -0,0 +1,13 @@
+package org.serviterobotics.friarbots;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BotsSplashActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
