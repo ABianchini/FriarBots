d744e4a8232aee76217c4062dc25b861f4a15474
Adriano Bianchini2 <animationbureau@gmail.com>
2012-01-27 22:44:50 -0800

    Made first item in menu, but it does not work as a button...

--- FriarBots/gen/org/serviterobotics/friarbots/R.java
+++ FriarBots/gen/org/serviterobotics/friarbots/R.java
@@ -16,9 +16,13 @@ public final class R {
     }
     public static final class color {
         public static final int black=0x7f050001;
+        public static final int grey=0x7f050002;
+        public static final int lightgrey=0x7f050003;
         public static final int white=0x7f050000;
     }
     public static final class dimen {
+        public static final int menu_text=0x7f060002;
+        public static final int menu_titles=0x7f060003;
         public static final int splash_padding=0x7f060000;
         public static final int title_text=0x7f060001;
     }
@@ -29,6 +33,7 @@ public final class R {
     }
     public static final class id {
         public static final int ImageView_SplashLogo=0x7f080001;
+        public static final int ScrollViewMenu=0x7f080004;
         public static final int TextView_SplashFRC=0x7f080000;
         public static final int TextView_TeamName=0x7f080003;
         public static final int TextView_TeamNum=0x7f080002;
@@ -36,6 +41,7 @@ public final class R {
     public static final class layout {
         public static final int begin=0x7f030000;
         public static final int menu=0x7f030001;
+        public static final int status=0x7f030002;
     }
     public static final class string {
         public static final int FRC=0x7f070002;
@@ -43,6 +49,8 @@ public final class R {
         public static final int frairbots=0x7f070004;
         public static final int hello=0x7f070000;
         public static final int menu=0x7f070005;
+        public static final int status=0x7f070006;
+        public static final int status_menu_text=0x7f070007;
         public static final int team=0x7f070003;
     }
 }
