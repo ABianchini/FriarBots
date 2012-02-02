731f9fed0977393c270d35ee013264bc6e265283
animationb <bianchini@advancementbureau.com>
2012-02-02 15:47:57 -0800

    JSON stuff

--- FriarBots/src/org/serviterobotics/friarbots/BotsActivity.java
+++ FriarBots/src/org/serviterobotics/friarbots/BotsActivity.java
@@ -1,9 +1,25 @@
 package org.serviterobotics.friarbots;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.util.Log;
 
 public class BotsActivity extends Activity {
 	public static final String SCOUT_DATA = "scoutData";
@@ -31,9 +47,37 @@ public class BotsActivity extends Activity {
         }).show();
     }
 	
-	public String readTeamInfoJSON() {
-		String builder = "build";
-		return builder;
+	public JSONObject readTeamInfo() {
+		StringBuilder builder = new StringBuilder();
+		HttpClient client = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet("http://twitter.com/statuses/user_timeline/vogella.json");
+		try {
+			HttpResponse response = client.execute(httpGet);
+			StatusLine statusLine = response.getStatusLine();
+			int statusCode = statusLine.getStatusCode();
+			if (statusCode == 200) {
+				HttpEntity entity = response.getEntity();
+				InputStream content = entity.getContent();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					builder.append(line);
+				}
+			} else {
+				Log.e(BotsActivity.class.toString(), "Failed to download file");
+			}
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			return new JSONObject(builder.toString());
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return new JSONObject();
+		}
 	}
 	
 }
